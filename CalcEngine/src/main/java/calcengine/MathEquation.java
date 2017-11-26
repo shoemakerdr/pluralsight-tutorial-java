742d0c6c35d1f98196964441e135fb02882995df
Derek Shoemaker <shoemakerdr@gmail.com>
2017-11-25 18:40:29 -0600

    Added constructors/initializers

--- CalcEngine/src/main/java/calcengine/MathEquation.java
+++ CalcEngine/src/main/java/calcengine/MathEquation.java
@@ -3,7 +3,7 @@ package main.java.calcengine;
 public class MathEquation {
     private double leftVal;
     private double rightVal;
-    private char opCode;
+    private char opCode = 'a';
     private double result;
 
     public double getLeftVal () {
@@ -32,6 +32,18 @@ public class MathEquation {
 
     public double getResult () {
         return result;
+    }
+
+    public MathEquation () {}
+
+    public MathEquation (char opCode) {
+        this.opCode = opCode;
+    }
+
+    public MathEquation (double leftVal, double rightVal, char opCode) {
+        this(opCode);
+        this.leftVal = leftVal;
+        this.rightVal = rightVal;
     }
 
     public void execute () {
